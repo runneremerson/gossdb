71ee2326ec281e742b10368cb2e5de0a107bfc3d
emerson <runner.emerson@gmail.com>
2017-01-18 17:56:09 +0800

    do some change

--- examples/test.go
+++ examples/test.go
@@ -1,31 +1,30 @@
 package main
 
 import (
-	"log"
 	"runtime"
-
-	"github.com/runneremerson/gossdb"
-	"github.com/runneremerson/gossdb/ssdb"
 )
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	gossdb.AuthPassword = ""
-	gossdb.Encoding = true
-	err := ssdb.Start()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	client, err := ssdb.Client()
-	if err != nil {
-		log.Println(err.Error())
-		return
-	}
-	defer client.Close()
-	client.Set("a", "hello1")
-	client.Set("b", "hello2")
-	client.Set("keys", "hello")
-	v, err := client.Hget("set", "key")
-	log.Println(v, err)
+	/*
+		gossdb.AuthPassword = ""
+		gossdb.Encoding = true
+		err := ssdb.Start()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		client, err := ssdb.Client()
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+		defer client.Close()
+		client.Set("a", "hello1")
+		client.Set("b", "hello2")
+		client.Set("keys", "hello")
+		v, err := client.Hget("set", "key")
+		log.Println(v, err)
+	*/
+	test3()
 }
